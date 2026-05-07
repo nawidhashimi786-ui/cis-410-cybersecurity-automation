89a89f489114504abb3c57fd9b546a29484aa895
Sayed Hashimi <sayedh19@students.highline.edu>
2026-05-06 21:58:51 -0700

    Added Terraform week6 configuration

---
+++ terraform/week6/main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "google" {
+  project = var.project_id
+  region  = var.region
+}
+
+resource "google_storage_bucket" "tf_bucket" {
+  name          = "${var.project_id}-bucket"
+  location      = var.region
+  force_destroy = true
+}
---
+++ terraform/week6/variables.tf
@@ -0,0 +1,11 @@
+variable "project_id" {
+  description = "GCP Project ID"
+  type        = string
+  default     = "cis410-sayed"
+}
+
+variable "region" {
+  description = "GCP Region"
+  type        = string
+  default     = "us-central1"
+}
